d1da31beedb5948cbd2571ef2fb57b9c9deb88ef
Roiqk7 <petrsevecek@icloud.com>
2024-07-19 15:33:06 +0200

    Reordered

--- include/invoker.hpp
+++ include/invoker.hpp
@@ -23,8 +23,6 @@ namespace CommandSystem
         */
         class Invoker                                                           // Singleton class
         {
-        public: // Variables
-                bool empty;                                                     // Flag to check if there are commands to process
         public: // Methods
         // Destructor
                 ~Invoker();
@@ -36,17 +34,19 @@ namespace CommandSystem
                 void waitCommand();                                            // Sleep until a command is submitted
                 void submit(pCommand command);                                 // Submit a command to the invoker
                 void process();                                                // Process the commands in the queue
+        private: // Methods
+                void init();                                                    // Initialize the invoker
+                void clean();                                                   // Clean up the invoker
+        protected: // Constructor
+                Invoker();                                                      // Constructor
+        public: // Variables
+                bool empty;                                                     // Flag to check if there are commands to process
         private: // Variables
         // Command system
                 std::mutex m_mutex;                                             // Mutex for the command queue
                 std::condition_variable m_conditionVariable;                    // Condition variable for the command queue
         // Command handling
                 CommandQueue m_commandQueue;                                    // Queue of commands ordered by priority
-        private: // Methods
-                void init();                                                    // Initialize the invoker
-                void clean();                                                   // Clean up the invoker
-        protected: // Constructor
-                Invoker();                                                      // Constructor
         };
 }
 
