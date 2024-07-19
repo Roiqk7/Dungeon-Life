b0d5cbf2bc2f9443c44c37f88b2c75ba49fa3424
Roiqk7 <petrsevecek@icloud.com>
2024-07-19 16:53:25 +0200

    Added health flag to better communicate the application state.

--- include/application.hpp
+++ include/application.hpp
@@ -11,6 +11,27 @@ Description: This file contains functions control the entire application.
 
 namespace Application
 {
+        /*
+        Healthy: Indicates the application is running normally without any
+                issues.
+        Degraded: Indicates the application is running but not at full capacity
+                or performance. This could be due to minor issues that don't
+                completely stop the application but might affect user experience
+                or efficiency.
+        Error: Indicates the application has encountered an error that has
+                stopped the application from running. This could be due to
+                critical issues that need to be resolved before the application
+                can continue running.
+        Critical: Indicates the application has encountered a critical error
+                and will be terminated as soon as possible.
+        ShuttingDown: Indicates the application is in the process of shutting
+                down and will be terminated.
+        */
+        enum class HealthFlag
+        {
+                Healthy = 0, Degraded, Error, Critical, ShuttingDown
+        };
+
         class Application
         {
         public: // Methods
@@ -22,11 +43,13 @@ namespace Application
                 void run();                                                     // Contains the main loop of the application
         private: // Methods
                 void init();                                                    // Initialize the application
-                bool checkHealth();                                             // Check the health of the application
-                bool checkAssets();                                             // Check if the assets are loaded
+                void controlFlow();                                             // Control the flow of the application
+                void manageHealth();                                            // Manage the health of the application
+                void checkHealth();                                             // Check the health of the application
+                bool resolveHealth();                                           // Resolve the health of the application
+                void exit();                                                    // Exit the application
         public: // Variables
-                bool continueRunning;                                           // Flag to determine if the application should keep running
-                bool terminate;                                                 // Flag to determine if the application should terminate
+                HealthFlag healthFlag;                                          // Flag to determine the health of the application
         private: // Variables
                 Exception::ExceptionHandler exceptionHandler;                   // Exception handler to handle all exceptions
         };
