ae8f5729e7240ea6b393fb982bc861276397124a
Roiqk7 <petrsevecek@icloud.com>
2024-07-19 15:14:29 +0200

    Made into a class

--- include/application.hpp
+++ include/application.hpp
@@ -7,12 +7,28 @@ Description: This file contains functions control the entire application.
 #ifndef APPLICATION_HPP
 #define APPLICATION_HPP
 
+#include "exception.hpp"
+#include "globals.hpp"
+
 namespace Application
 {
-        void initApplication();                                                 // Checks if the application is ready to run and initializes it
-        void runApplication();                                                  // Runs the application loop
-        void endApplication();                                                  // Ends the application gracefully
-        void exitApplication();                                                 // Exits the application immediately
+        class Application
+        {
+        public: // Variables
+                bool continueRunning;                                           // Flag to determine if the application should keep running
+                bool terminate;                                                 // Flag to determine if the application should terminate
+        public: // Methods
+        // Constructor
+                Application();
+        // Destructor
+                ~Application();
+        // Application control
+                void run();                                                     // Run the application
+        private: // Methods
+                void init();                                                    // Initialize the application
+                bool checkHealth();                                             // Check the health of the application
+                bool checkAssets();                                             // Check if the assets are loaded
+        };
 }
 
 #endif // !APPLICATION_HPP
