b11b8cb6283f4880f049c7f2efc9ea0500929821
Roiqk7 <petrsevecek@icloud.com>
2024-07-19 15:31:15 +0200

    Added exception handler and reordered

--- include/application.hpp
+++ include/application.hpp
@@ -7,16 +7,12 @@ Description: This file contains functions control the entire application.
 #ifndef APPLICATION_HPP
 #define APPLICATION_HPP
 
-#include "exception.hpp"
-#include "globals.hpp"
+#include "exceptionHandler.hpp"
 
 namespace Application
 {
         class Application
         {
-        public: // Variables
-                bool continueRunning;                                           // Flag to determine if the application should keep running
-                bool terminate;                                                 // Flag to determine if the application should terminate
         public: // Methods
         // Constructor
                 Application();
@@ -28,6 +24,11 @@ namespace Application
                 void init();                                                    // Initialize the application
                 bool checkHealth();                                             // Check the health of the application
                 bool checkAssets();                                             // Check if the assets are loaded
+        public: // Variables
+                bool continueRunning;                                           // Flag to determine if the application should keep running
+                bool terminate;                                                 // Flag to determine if the application should terminate
+        private: // Variables
+                Exception::ExceptionHandler exceptionHandler;                   // Exception handler to handle all exceptions
         };
 }
 
