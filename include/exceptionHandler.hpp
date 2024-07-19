e5856bf076be04b65e5f8a6fa27255a8e752b278
Roiqk7 <petrsevecek@icloud.com>
2024-07-19 15:32:02 +0200

    Added

---
+++ include/exceptionHandler.hpp
@@ -0,0 +1,28 @@
+/*
+Date: 19/07/2024
+
+Description: This file contains the exception handler class which is used to handle all exceptions in the application.
+*/
+
+#ifndef EXCEPTION_HANDLER_HPP
+#define EXCEPTION_HANDLER_HPP
+
+#include "exception.hpp"
+
+namespace Exception
+{
+        class ExceptionHandler
+        {
+        public: // Methods
+        // Constructor
+                ExceptionHandler();
+        // Exception handling
+                void processExceptions();                                       // Process all exceptions in the queue
+        private: // Methods
+                void handleException(pException& exception);                    // Handle a specific exception
+        public: // Variables
+                ExceptionQueue exceptions;                                      // Queue of unprocessed exceptions
+        };
+}
+
+#endif // !EXCEPTION_HANDLER_HPP
