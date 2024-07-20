4d1b85906e38ad8c1e00cc1ccbd62d4ebdc79ede
Roiqk7 <petrsevecek@icloud.com>
2024-07-20 14:59:53 +0200

    Removed type from command.hpp and moved it to Globals so it can be used with tasks as well

--- include/command.hpp
+++ include/command.hpp
@@ -19,34 +19,26 @@ namespace CommandSystem
         using namespace Globals;
 
         /*
-        Command types are helpful for the invoker to correctly process the commands in the queue.
-        */
-        enum class CommandType
-        {
-                None = 0, UserInput, System, Exception
-        };
-
-        /*
         The command class is an abstract class which is inherited by all the commands.
         */
         class Command
         {
         public: // Methods
         // Constructor
-                Command(std::string name, CommandType commandType, Priority priority);
+                Command(std::string name, Type commandType, Priority priority);
         // Virtual methods
                 virtual ~Command() = default;
                 virtual void execute() = 0;
         // Non-virtual methods
         // Getters
                 std::string getName() const;                                    // Get the name of the command
-                CommandType getCommandType() const;                             // Get the type of the command
+                Type getType() const;                                           // Get the type of the command
                 Priority getPriority() const;                                   // Get the priority of the command
         // Operators
                 bool operator<(const Command& other) const;                     // Less than operator
         protected: // Variables
                 std::string m_name;                                             // Name of the command
-                CommandType m_commandType;                                      // Command type
+                Type m_type;                                                    // Command type
                 Priority m_priority;                                            // Command priority
         };
 
