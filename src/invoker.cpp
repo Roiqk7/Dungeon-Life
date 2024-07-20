1a73e255295c18f151812a8804a34bed9697832e
Roiqk7 <petrsevecek@icloud.com>
2024-07-20 10:41:40 +0200

    Implemented all invoker methods

--- src/invoker.cpp
+++ src/invoker.cpp
@@ -14,8 +14,8 @@ namespace CommandSystem
         */
         Invoker::~Invoker()
         {
-                // Clean up the invoker
-                clean();
+                // Log the destruction of the invoker
+                LOG_INFO("Invoker instance destroyed.");
         }
 
         /*
@@ -27,5 +27,87 @@ namespace CommandSystem
         {
                 static Invoker instance;
                 return instance;
+        }
+
+        /*
+        Sleep until a command is submitted.
+        */
+        void Invoker::waitCommand()
+        {
+                // Lock the mutex
+                std::unique_lock<std::mutex> lock(m_commandQueue.mutex);
+
+                // Wait until the queue is not empty
+                m_commandQueue.notEmptyCondition.wait(lock, [this] { return !m_commandQueue.empty(); });
+        }
+
+        /*
+        Submit a command to the invoker.
+
+        @param command: The command to submit.
+        */
+        void Invoker::submit(pCommand command)
+        {
+                // Push the command to the queue
+                m_commandQueue.push(std::move(command));
+        }
+
+        /*
+        Process all the commands in the queue.
+        */
+        void Invoker::process()
+        {
+                // While the queue is not empty
+                while(!m_commandQueue.empty())
+                {
+                        // Process the command
+                        processCommand(m_commandQueue.pop());
+                }
+        }
+
+        /*
+        Process the commands in the queue given a duration.
+
+        @param duration: The duration to process the commands.
+        */
+        void Invoker::process(const std::chrono::microseconds& duration)
+        {
+                // Get the start time
+                auto startTime = std::chrono::high_resolution_clock::now();
+
+                // While the queue is not empty and the duration has not elapsed
+                // TODO: Make the duration check into a function
+                while(!m_commandQueue.empty() && std::chrono::high_resolution_clock::now() - startTime < duration)
+                {
+                        // Process the command
+                        processCommand(m_commandQueue.pop());
+                }
+        }
+
+        /*
+        Constructor. Initializes the invoker.
+        */
+        Invoker::Invoker()
+        {
+                // Log the creation of the invoker
+                LOG_INFO("Invoker instance created.");
+        }
+
+        /*
+        Process a command.
+
+        @param command: The command to process.
+        */
+        void Invoker::processCommand(pCommand command)
+        {
+                // Check if the command is valid
+                if (command)
+                {
+                        // Log the command execution
+                        LOG_TRACE("Executing command: " + command->getName());
+
+                        // Execute the command
+                        command->execute();
+                }
         }
 }
