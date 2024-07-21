dba55fdc25d999da372865a91b2f6c1266458777
Roiqk7 <petrsevecek@icloud.com>
2024-07-21 10:05:33 +0200

    Added

---
+++ src/threadManager.cpp
@@ -0,0 +1,113 @@
+/*
+Date: 21/07/2024
+
+Description: This file implements the thread manager class which is responsible for managing threads.
+*/
+
+#include <thread>
+#include <vector>
+#include "../include/priorityQueue.hpp"
+#include "../include/threadManager.hpp"
+
+namespace ThreadManager
+{
+        /*
+        Destructor which is responsible for shutting down all threads.
+        */
+        ThreadManager::~ThreadManager()
+        {
+                // Shut down the thread manager
+                shutDown();
+        }
+
+        /*
+        Singleton.
+
+        @return The thread manager instance.
+        */
+        ThreadManager& ThreadManager::getInstance()
+        {
+                static ThreadManager instance;
+                return instance;
+        }
+
+        /*
+        Submit a task to the thread manager.
+
+        @param task: The task to submit.
+        */
+        void ThreadManager::submit(pTask task)
+        {
+                // Add the task to the queue
+                m_queue.push(std::move(task));
+        }
+
+        /*
+        Constructor.
+        */
+        ThreadManager::ThreadManager()
+                : m_maxThreads(std::thread::hardware_concurrency()), m_shutDown(false)
+        {
+                // Create the task handler thread
+                m_taskHandler = std::thread(&ThreadManager::handleTask, this);
+
+                // Create single worker thread
+                m_threads.push_back(std::thread(&ThreadManager::worker, this));
+        }
+
+        /*
+        Handle a task.
+        */
+        void ThreadManager::handleTask()
+        {
+                // Handle tasks until the thread manager is shut down
+                while (!m_shutDown)
+                {
+                        // Get the next task
+                        pTask task = m_queue.pop();
+
+                        // Execute the task
+                        task->execute();
+                }
+        }
+
+        /*
+        Worker thread function.
+        */
+        void ThreadManager::worker()
+        {
+                // Work until the thread manager is shut down
+                while (!m_shutDown)
+                {
+                        // Lock the mutex
+                        std::unique_lock<std::mutex> lock(m_queue.mutex);
+
+                        // Wait until the queue is not empty
+                        m_queue.notEmptyCondition.wait(lock, [this] { return !m_queue.empty(); });
+
+                        // Get the next task
+                        pTask task = m_queue.pop();
+
+                        // Execute the task
+                        task->execute();
+                }
+        }
+
+        /*
+        Shut down the thread manager.
+        */
+        void ThreadManager::shutDown()
+        {
+                // Set the shut down flag
+                m_shutDown = true;
+
+                // Join the worker threads
+                for (auto& thread : m_threads)
+                {
+                        thread.join();
+                }
+
+                // Join the task handler thread
+                m_taskHandler.join();
+        }
+}
