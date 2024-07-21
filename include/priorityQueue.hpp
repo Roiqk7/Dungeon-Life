3c25f33b185b15fdd9ec7c5581b3f0c63afb454c
Roiqk7 <petrsevecek@icloud.com>
2024-07-21 14:49:47 +0200

    Redefined underlying queue

--- include/priorityQueue.hpp
+++ include/priorityQueue.hpp
@@ -7,6 +7,7 @@ Description: This file contains the priority queue class template which is respo
 #ifndef PRIORITY_QUEUE_HPP
 #define PRIORITY_QUEUE_HPP
 
+#include <array>
 #include <condition_variable>
 #include <mutex>
 #include <queue>
@@ -31,13 +32,15 @@ namespace Tool
                 void push(const pPriorityQueueElement& element);                // Push an element to the queue
                 pPriorityQueueElement pop();                                    // Pop an element from the queue
                 bool empty() const;                                             // Check if the queue is empty
+        private: // Methods
+                size_t getPriorityIndex(Globals::Priority priority) const;      // Get the index of the priority in the queue
         public: // Variables
                 mutable std::mutex mutex;                                       // Mutex to protect the queue (thread-safe)
                 std::condition_variable notEmptyCondition;                      // Condition variable to notify when the queue is not empty
         private: // Variables
-                std::priority_queue<pPriorityQueueElement,
-                        std::vector<pPriorityQueueElement>,
-                        pPriorityQueueElementComparator> queue;                 // Priority queue
+                // Note: Most important priority is at the front of the array
+                std::array<std::queue<pPriorityQueueElement>,
+                        static_cast<size_t>(Globals::Priority::Size)> queue;    // The priority queue (array of queues)
         };
 }
 
--- src/priorityQueue.cpp
+++ src/priorityQueue.cpp
@@ -5,64 +5,115 @@ Description: This file implements the priority queue class template which is res
 */
 
 #include <condition_variable>
+#include <math.h>
 #include <mutex>
 #include <queue>
 #include <vector>
 #include "../include/priorityQueue.hpp"
+#include "../include/priorityQueueElement.hpp"
+#include "../include/threadManager.hpp"
 
 namespace Tool
 {
         /*
         Push an element to the queue.
 
-        @param element: The element to push to the queue.
+        @param element The element to push.
         */
-        template<typename T, typename Comparator>
-        void PriorityQueue<T, Comparator>::push(const T& element)
+        void PriorityQueue::push(const pPriorityQueueElement& element)
         {
-                // Lock the mutex
-                std::lock_guard<std::mutex> lock(mutex);
+                // Check the multi-threaded flag
+                if (ThreadManager::ThreadManager::getInstance().multiThreaded)
+                {
+                        // Lock the mutex
+                        std::lock_guard<std::mutex> lock(mutex);
+                }
+
+                // Calculate the index of the priority
+                size_t index = getPriorityIndex(element->getPriority());
 
                 // Push the element to the queue
-                queue.push(element);
+                queue[index].push(std::move(element));
 
-                // Notify the condition variable that the queue is not empty
+                // Notify that the queue is not empty
                 notEmptyCondition.notify_one();
         }
 
         /*
         Pop an element from the queue.
 
-        @return: The element that was popped from the queue. If the queue is empty, a default-constructed element is returned.
+        @return The element popped from the queue. If the queue is empty, a nullptr is returned.
         */
-        template<typename T, typename Comparator>
-        T PriorityQueue<T, Comparator>::pop()
+        pPriorityQueueElement PriorityQueue::pop()
         {
-                // Lock the mutex
-                std::unique_lock<std::mutex> lock(mutex);
+                // Check the multi-threaded flag
+                if (ThreadManager::ThreadManager::getInstance().multiThreaded)
+                {
+                        // Lock the mutex
+                        std::lock_guard<std::mutex> lock(mutex);
+                }
 
-                // Wait until the queue is not empty
-                notEmptyCondition.wait(lock, [this] { return !empty(); });
+                // Iterate through the queue
+                for (auto& q : queue)
+                {
+                        // Check if the queue is not empty
+                        if (!q.empty())
+                        {
+                                // Get the top element from the queue
+                                pPriorityQueueElement element = std::move(q.front());
 
-                // Get the top element from the queue
-                T element = std::move(queue.top());
-                queue.pop();
+                                // Pop the element from the queue
+                                q.pop();
 
-                return element;
+                                // Return the element
+                                return element;
+                        }
+                }
+
+                // Return a nullptr
+                return nullptr;
         }
 
         /*
         Check if the queue is empty.
 
-        @return: True if the queue is empty, false otherwise.
+        @return True if the queue is empty, false otherwise.
         */
-        template<typename T, typename Comparator>
-        bool PriorityQueue<T, Comparator>::empty() const
+        bool PriorityQueue::empty() const
         {
-                // Lock the mutex
-                std::lock_guard<std::mutex> lock(mutex);
+                // Iterate through the queue
+                for (const auto& q : queue)
+                {
+                        // Check if the queue is not empty
+                        if (!q.empty())
+                        {
+                                // Return false
+                                return false;
+                        }
+                }
 
-                // Check if the queue is empty
-                return queue.empty();
+                // Return true
+                return true;
+        }
+
+
+        /*
+        Get the index of the priority in the queue.
+
+        @param priority The priority to get the index of.
+
+        @return The index of the priority in the queue.
+        */
+        size_t PriorityQueue::getPriorityIndex(Globals::Priority priority) const
+        {
+                // Convert the priority to size_t
+                const size_t convertedPriority = static_cast<size_t>(priority);
+
+                // Convert the Globals::Priority::Size to size_t
+                const size_t convertedSize = static_cast<size_t>(Globals::Priority::Size);
+
+                // Return the index of the priority in the queue
+                // Note: This means that the highest priority is at the front of the array
+                return static_cast<size_t>(convertedSize - convertedPriority);
         }
 }
