063acec4e2e09e44088009ffe3d10848974c8357
Roiqk7 <petrsevecek@icloud.com>
2024-07-21 13:26:09 +0200

    Added handler inheritance

--- include/frameHandler.hpp
+++ include/frameHandler.hpp
@@ -10,6 +10,7 @@ Description: This file defines a singleton class which handles the frame rate of
 #include <chrono>
 #include <thread>
 #include "globals.hpp"
+#include "handler.hpp"
 
 namespace FrameHandler
 {
@@ -17,19 +18,14 @@ namespace FrameHandler
         Calculates the delta time between frames. Makes sure the app runs at a
                 constant frame rate.
         */
-        class FrameHandler
+        class FrameHandler : public Handler::Handler<FrameHandler>
         {
         public: // Methods
-        // Singleton
-                static FrameHandler& getInstance();                             // Get the instance of the frame handler
-                FrameHandler(const FrameHandler&) = delete;                     // Delete copy constructor
-                FrameHandler& operator=(const FrameHandler&) = delete;          // Delete copy assignment operator
         // Frame handling
                 void handleFrame();                                             // Handle the frame (sleep if necessary)
                 std::chrono::microseconds getDeltaTime();                       // Get the delta time
                 std::chrono::microseconds getRemainingTime();                   // Get the remaining time until the next frame
         private: // Methods
-                void init();                                                    // Initialize the frame handler
                 void calculateDeltaTime();                                      // Calculate the delta time
         private: // Variables
                 std::chrono::time_point<std::chrono::high_resolution_clock>
