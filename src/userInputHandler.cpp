c614a6a148e1afd4d291b7c8158b624ba5c6f34a
Roiqk7 <petrsevecek@icloud.com>
2024-07-23 14:45:02 +0200

    Added basic structure

--- src/userInputHandler.cpp
+++ src/userInputHandler.cpp
@@ -4,6 +4,8 @@ Date: 23/07/2024
 Description: This file implements the user input handler class which is responsible for handling user input.
 */
 
+#include <SDL2/SDL.h>
+#include "../include/application.hpp"
 #include "../include/globals.hpp"
 #include "../include/invoker.hpp"
 #include "../include/threadManager.hpp"
@@ -16,6 +18,70 @@ namespace UserInput
         */
         void UserInputHandler::waitForUserInput()
         {
-                // TODO: Implement
+                // Initialize the event
+                SDL_Event event;
+
+                // SDL_WaitEvent will block until an event is available
+                SDL_WaitEvent(&event);
+
+                // Handle the event
+                switch (event.type)
+                {
+                        // Quit event
+                        case SDL_QUIT:
+                        {
+                                // Shut down the application
+                                Application::Application::shouldClose = true;
+                                break;
+                        }
+                        // Keyboard event
+                        case SDL_KEYDOWN:
+                        {
+                                // Process the key event
+                                // TODO: processUserInput(UserInput(event.key.keysym.sym));
+                                break;
+                        }
+                        // Mouse event
+                        case SDL_MOUSEBUTTONDOWN:
+                        {
+                                // Process the mouse event
+                                // TODO: processUserInput(UserInput(event.button.button));
+                                break;
+                        }
+                }
+        }
+
+        /*
+        Process user input.
+        */
+        void UserInputHandler::processUserInput(const UserInput& input)
+        {
+                // Validate the user input
+                if (validateUserInput(input))
+                {
+                        // Parse the user input and create a command
+                        // TODO: Tool::pPriorityQueueElement command = parseUserInput(input);
+
+                        // Submit the command to the invoker
+                        // TODO: CommandSystem::Invoker::getInstance().submitTask(command);
+                }
+        }
+
+        /*
+        Validate user input.
+        */
+        bool UserInputHandler::validateUserInput(const UserInput& input)
+        {
+                // TODO: Implement this function
+                return false;
+        }
+
+        /*
+        Parse user input and create a command.
+        */
+        Tool::pPriorityQueueElement UserInputHandler::parseUserInput(const UserInput& input)
+        {
+                // TODO: Implement this function
+                return nullptr;
         }
 }
