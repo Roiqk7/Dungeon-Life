6a68da266b6d971423b593865257fbed7687d001
Roiqk7 <petrsevecek@icloud.com>
2024-07-23 10:40:24 +0200

    Implemented the new requestThread() and tweaked the other functions to follow for the new architecture.

--- src/threadManager.cpp
+++ src/threadManager.cpp
@@ -52,7 +52,13 @@ namespace ThreadManager
                 // Set the shut down flag
                 m_shutDown = true;
 
-                // Join the worker threads
+                // Join the main thread
+                if (m_mainThread.joinable())
+                {
+                        m_mainThread.join();
+                }
+
+                // Join the other threads
                 for (auto& thread : m_threads)
                 {
                         // Check if the thread is joinable
@@ -65,12 +71,37 @@ namespace ThreadManager
         }
 
         /*
+        Request a thread.
+
+        @param func The function to execute in the thread
+        @param args The arguments to pass to the function
+        */
+        template<typename Func, typename... Args>
+        void ThreadManager::requestThread(Func&& func, Args&&... args)
+        {
+                // Check the shutdown flag
+                if (!m_shutDown)
+                {
+                        // Check if the number of threads is less than the maximum
+                        // Note: We add 1 as it represents the main thread
+                        if (m_threads.size() + 1 <= m_maxThreads)
+                        {
+                                // Create a new thread
+                                m_threads.push_back(std::thread(func, args...));
+
+                                // Update the multi-threaded flag
+                                multiThreaded = true;
+                        }
+                }
+        }
+
+        /*
         Initialize the thread manager.
         */
         void ThreadManager::init()
         {
-                // Initialize the task handler thread with single main thread
-                m_threads.push_back(std::thread(&ThreadManager::worker, this));
+                // Initialize the main thread
+                m_mainThread = std::thread(&ThreadManager::worker, this);
         }
 
         /*
