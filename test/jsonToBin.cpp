19426c853de0552104877f99409ffb452badbea5
Roiqk7 <petrsevecek@icloud.com>
2024-07-24 08:10:38 +0200

    Added #error

--- test/jsonToBin.cpp
+++ test/jsonToBin.cpp
@@ -4,14 +4,21 @@ Date: 20/07/2024
 Description: This file contains tests for the jsonToBin.py tool.
 */
 
-#ifdef TEST
+#ifndef TEST
+#error "Tried to compile a test file without the TEST macro defined. Aborting compilation."
+#else
 
-#include <gtest/gtest.h>
 #include <json/json.h>
+#include <gtest/gtest.h>
+#include <string>
 
 #ifdef DEVELOPMENT
 #include <spdlog/spdlog.h>
 #endif // DEVELOPMENT
+
+// Constants
+const std::string JSON_TO_BIN_PATH = "tool/jsonToBin.py";
+const std::string BIN_TO_JSON_PATH = "tool/binToJson.py";
 
 // Test for converting a JSON scene to a binary file
 TEST(JsonToBin, jsonToBin)
