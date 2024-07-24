f1bfcf6075df158126781236aee1946c40724353
Roiqk7 <petrsevecek@icloud.com>
2024-07-24 08:19:56 +0200

    Added

---
+++ test/include/globals.hpp
@@ -0,0 +1,17 @@
+/*
+Date: 24/07/2024
+
+Description: 
+*/
+
+#ifndef TEST_GLOBALS_HPP
+#define TEST_GLOBALS_HPP
+
+#include <string>
+
+// Constants
+// jsonToBin.cpp
+const std::string JSON_TO_BIN_PATH = "tool/jsonToBin.py";
+const std::string BIN_TO_JSON_PATH = "tool/binToJson.py";
+
+#endif // !TEST_GLOBALS_HPP
