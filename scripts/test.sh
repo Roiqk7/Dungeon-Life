652dc61614b708de7aed1c8ffb2e5ba8f3144241
Roiqk7 <petrsevecek@icloud.com>
2024-07-26 06:22:06 +0200

    Added empty checks

--- scripts/test.sh
+++ scripts/test.sh
@@ -8,23 +8,31 @@ echo "Checking if the build was successful..."
 # Initialize the build status flag optimistically
 build_success=1
 
-# Check necessary dependencies
+# Check the necessary dependencies
 # Check if homebrew is installed
 if ! command -v brew >/dev/null 2>&1; then
         echo "[Error] Homebrew is not installed."
         build_success=0
 fi
 
-# Check necessary directories and files
+# Check the necessary directories and files
 # Check if the bin directory exists
 if [ ! -d "bin" ]; then
         echo "[Error] The bin directory does not exist."
+        build_success=0
+# Check if the bin directory is empty
+elif [ -z "$(ls -A bin)" ]; then
+        echo "[Error] The bin directory is empty."
         build_success=0
 fi
 
 # Check if the build directory exists
 if [ ! -d "build" ]; then
         echo "[Error] The build directory does not exist."
+        build_success=0
+# Check if the build directory is empty
+elif [ -z "$(ls -A build)" ]; then
+        echo "[Error] The build directory is empty."
         build_success=0
 fi
 
