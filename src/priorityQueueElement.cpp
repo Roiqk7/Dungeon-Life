6fb465afd1f113c38a5d60ce9d47065c89e817e2
Roiqk7 <petrsevecek@icloud.com>
2024-07-28 08:45:14 +0200

    Added

---
+++ src/priorityQueueElement.cpp
@@ -0,0 +1,111 @@
+/*
+Date: 21/07/2024
+
+Description: This file implements the priority queue element class which is used to store elements in a priority queue.
+*/
+
+#include <functional>
+#include <memory>
+#include <string>
+#include "../include/globals.hpp"
+#include "../include/priorityQueueElement.hpp"
+
+namespace Tool
+{
+        #ifdef TEST
+        /*
+        Constructor. Should be used only for testing.
+
+        @param name Name of the element
+        @param priority Priority of the element
+        */
+        PriorityQueueElement(const std::string& name,
+                Globals::Priority priority)
+                : m_name(name), m_priority(priority), m_type(Globals::Type::Test), m_callback([](){}) {}
+        #endif // TEST
+
+        /*
+        Constructor.
+
+        @param name Name of the element
+        @param priority Priority of the element
+        @param type Type of the element
+        @param callback Callback function to execute when the element is processed
+        */
+        PriorityQueueElement::PriorityQueueElement(const std::string& name,
+                Globals::Priority priority,
+                Globals::Type type, std::function<void()> callback)
+                : m_name(name), m_priority(priority), m_type(type), m_callback(callback) {}
+
+        /*
+        Get the name of the element.
+
+        @return Name of the element
+        */
+        const std::string& PriorityQueueElement::getName() const
+        {
+                return m_name;
+        }
+
+        /*
+        Get the priority of the element.
+
+        @return Priority of the element
+        */
+        Globals::Priority PriorityQueueElement::getPriority() const
+        {
+                return m_priority;
+        }
+
+        /*
+        Get the type of the element.
+
+        @return Type of the element
+        */
+        Globals::Type PriorityQueueElement::getType() const
+        {
+                return m_type;
+        }
+
+        /*
+        Clone the element.
+
+        @return A unique pointer to a copy of the element
+        */
+        pPriorityQueueElement PriorityQueueElement::clone() const
+        {
+                return std::make_unique<PriorityQueueElement>(m_name, m_priority, m_type, m_callback);
+        }
+
+        /*
+        Execute the callback function.
+        */
+        void PriorityQueueElement::execute()
+        {
+                m_callback();
+        }
+
+        /*
+        Equality operator.
+
+        @param other Other element to compare
+
+        @return True if the elements are equal, false otherwise
+        */
+        bool PriorityQueueElement::operator==(const PriorityQueueElement& other) const
+        {
+                return m_name == other.m_name && m_priority == other.m_priority;
+        }
+
+        /*
+        Less than operator.
+
+        @param other Other element to compare
+
+        @return True if this element is less than the other, false otherwise
+        */
+        bool PriorityQueueElement::operator<(const PriorityQueueElement& other) const
+        {
+                return m_priority < other.m_priority;
+        }
+}
