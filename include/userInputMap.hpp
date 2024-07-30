f1f5ba6b691f910cc9ac115662457f9f2b6a6fd0
Roiqk7 <petrsevecek@icloud.com>
2024-07-30 08:44:59 +0200

    Added

---
+++ include/userInputMap.hpp
@@ -0,0 +1,20 @@
+/*
+Date: 30/07/2024
+
+Description: This file contains the user input map class which is responsible for mapping user input to commands.
+*/
+
+#include "../include/application.hpp"
+#include "../include/invoker.hpp"
+#include "../include/userInput.hpp"
+
+namespace UserInput
+{
+        /*
+        Map user input to commands.
+        */
+        class UserInputMap
+        {
+                // TODO: Implement the UserInputMap class
+        };
+}
