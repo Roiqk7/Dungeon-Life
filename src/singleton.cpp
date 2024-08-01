fa778b84870f674c0fa5b026a80dcbbd5bcf4fc5
Roiqk7 <petrsevecek@icloud.com>
2024-08-01 19:36:09 +0200

    Updated

--- src/singleton.cpp
+++ src/singleton.cpp
@@ -17,7 +17,34 @@ namespace Tool
                 // Create the instance of the singleton class
                 static Derived instance;
 
+                // Register the instance of the singleton class
+                registerInstance(std::make_unique<Derived>(instance));
+
                 // Return the instance of the singleton class
                 return instance;
+        }
+
+        /*
+        Get the instances of the singleton class.
+
+        @return Instances of the singleton class
+        */
+        template<typename Derived>
+        const std::vector<std::unique_ptr<Derived>>& Singleton<Derived>::getInstances()
+        {
+                // Return the instances of the singleton class
+                return m_instances;
+        }
+
+        /*
+        Register the instance of the singleton class.
+
+        @param instance Instance of the singleton class
+        */
+        template<typename Derived>
+        void Singleton<Derived>::registerInstance(std::unique_ptr<Derived> instance)
+        {
+                // Add the instance to the vector of instances
+                m_instances.push_back(std::move(instance));
         }
 }
