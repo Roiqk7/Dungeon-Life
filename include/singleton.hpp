7b4efc1b7095a3993f961a13b20c4f02743211e2
Roiqk7 <petrsevecek@icloud.com>
2024-08-01 19:33:50 +0200

    Updated

--- include/singleton.hpp
+++ include/singleton.hpp
@@ -7,6 +7,9 @@ Description: This file contains the singleton base class from which other single
 #ifndef SINGLETON_HPP
 #define SINGLETON_HPP
 
+#include <memory>
+#include <vector>
+
 namespace Tool
 {
         /*
@@ -22,10 +25,22 @@ namespace Tool
                 static Derived& getInstance();
                 Singleton(const Singleton&) = delete;
                 Singleton& operator=(const Singleton&) = delete;
+        // Management
+                static const std::vector<std::unique_ptr<Derived>>& getInstances();
         protected: // Methods
         // Constructor
                 Singleton() = default;
+        private: // Methods
+        // Management
+                static void registerInstance(std::unique_ptr<Derived> instance);
+        private: // Variables
+        // Management
+                static std::vector<std::unique_ptr<Derived>> m_instances;
         };
+
+        // Initialize the static vector
+        template<typename Derived>
+        std::vector<std::unique_ptr<Derived>> Singleton<Derived>::m_instances;
 }
 
 #endif // !SINGLETON_HPP
